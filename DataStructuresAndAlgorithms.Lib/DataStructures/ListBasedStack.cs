032eb7f9ac9a1156701bb6fd803a061b00118ce7
Rodney Cabahug <rodney.cabahug@gmail.com>
2022-04-18 02:51:35 +1200

    refactor: Replace with own implementation of list Used own DoublyLinkedList implementation.

--- DataStructuresAndAlgorithms.Lib/DataStructures/ListBasedStack.cs
+++ DataStructuresAndAlgorithms.Lib/DataStructures/ListBasedStack.cs
@@ -6,25 +6,28 @@ public class ListBasedStack<T> : IStack<T>
 {
     private const string _emptyErrorMessage = "Stack is empty.";
 
-    private readonly LinkedList<T> _stack;
+    private readonly DoublyLinkedList<T> _stack;
 
     public ListBasedStack()
     {
-        _stack = new LinkedList<T>();
+        _stack = new DoublyLinkedList<T>();
     }
 
     public int Size => _stack.Count;
 
-    public bool IsEmpty => _stack.Count == 0;
+    public bool IsEmpty => _stack.IsEmpty;
 
     public void Clear() => _stack.Clear();
 
     public bool Contains(T item) => _stack.Contains(item);
 
-    public T Peek() =>
-        IsEmpty 
-            ? throw new InvalidOperationException(_emptyErrorMessage) 
-            : _stack.Last();
+    public T Peek()
+    {
+        if (_stack.IsEmpty)
+            throw new InvalidOperationException(_emptyErrorMessage);
+
+        return _stack.Last;
+    }
 
 
     public T Pop()
